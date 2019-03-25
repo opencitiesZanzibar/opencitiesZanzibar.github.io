157adc15866087ce3cf9215e8e08c2336f7b73f4
opencitiesZanzibar <48554362+opencitiesZanzibar@users.noreply.github.com>
2019-03-25 14:24:14 +0300

    Update ocMap1.js

--- js/ocMap1.js
+++ js/ocMap1.js
@@ -626,7 +626,7 @@ var overlays = {
         };
 var basemaps = {
             
-            "Satellite":ZMImagelayer,
+            "ZMI Image":ZMImagelayer,
             "OpenStreetMap":osmlayer
         };
 
