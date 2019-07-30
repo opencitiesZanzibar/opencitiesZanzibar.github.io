5251681c0216a73ba796571cbd0caa56186aa8a8
opencitiesZanzibar <48554362+opencitiesZanzibar@users.noreply.github.com>
2019-07-30 17:02:46 +0300

    Update ocMap1.js

--- js/ocMap1.js
+++ js/ocMap1.js
@@ -1,5 +1,5 @@
 
-var map = new L.map('map',{ zoomControl: false }).setView([-6.168513, 39.204011], 16 );
+var map = new L.map('map',{ zoomControl: false }).setView([-6.168513, 39.204111], 16 );
 
 //map.setMaxBounds(map.getBounds());
 
